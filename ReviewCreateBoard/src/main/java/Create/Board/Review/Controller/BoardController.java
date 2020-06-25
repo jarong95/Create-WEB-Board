2f5fcdee79e0173b74e6730b1919848710ab0ef8
jarong95 <48700194+jarong95@users.noreply.github.com>
2020-06-25 18:08:13 +0900

    수정 주석
    수정 ㅅ주석 다는 중1

--- ReviewCreateBoard/src/main/java/Create/Board/Review/Controller/BoardController.java
+++ ReviewCreateBoard/src/main/java/Create/Board/Review/Controller/BoardController.java
@@ -34,37 +34,53 @@ import Create.Board.Review.VO.ReplyVO;
 @Controller
 public class BoardController {
 
+	/*
+	 * 각 상황의 타입에 맞는 IoC컨테이너 안에 존재하는 Bean을 자동으로 주입해준다. 
+	 * @Repository Dao라는 것을 알리기 위해 사용하하는데 이를 사용하기 위해 Autowitred를 사용한다. 
+	 */
 	@Autowired
-	BoardDAO dao;
+	BoardDAO dao; //BoardDAO를 사용하기 위해 
 	
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
-	
+	//로거를 사용하기 위해 현재 상황을 파악하기 위한 목적으로 사용한다. 
+	//System.out.println();을 사용이 불가하기 때문
 	/*
 	 * 
+	
 	//페이지 당 글 수 
 	private static final int countPerPage = 10;
 		
 	//페이지 이동 링크 그룹당 페이지 수
 	private static final int pagePerGroup = 10;
 	*/
+	
 	private final String uploadPath = "/boardfile";
+	//파일을저장하기위한 경로 설정
 	
 /*	@RequestMapping(value="write", method=RequestMethod.GET)
 	public String write(){
 		return "write";
 	}*/
 	
+	
+	//글쓰기 post로 받으음
 	@RequestMapping(value="write", method=RequestMethod.POST)
 	public String wrtie(BoardVO bv
 			, MultipartFile upload1){
+		//BoardVO 객체로 받고 파일은 multipartfile로 받음
 		
 		if (upload1 != null && !upload1.isEmpty()) {
+			//파일이 있는 경우 fileservice를 이용하여 저장
 			String savedfile = FileService.saveFile(upload1, uploadPath);
+			//저장한 파일이름과 파일 원본 이름을 저장
 			bv.setFilename(upload1.getOriginalFilename());
 			bv.setFilesavename(savedfile);
 		}
 		
+		//dao로 이동시켜서 DB에 저장
 		dao.writeBoard(bv);
+		
+		//board 주소로 redirect
 		return "redirect:board";
 	}
 	
@@ -94,14 +110,21 @@ public class BoardController {
 		return "read";
 	}
 	 */
+	
+	
 	@RequestMapping(value="board", method=RequestMethod.GET)
 	public String board(Model model){
 		
+		//게시글과 답글을 모두 가져온다.
 		ArrayList<BoardVO> list = dao.getBoard();
 		ArrayList<ReplyVO> resultList = dao.getReply();
 		logger.debug("{}", resultList);
+		
+		//model객체에 저장하여 화면에 출력한다.
 		model.addAttribute("list_board", list);
 		model.addAttribute("reply", resultList);
+		
+		//groupBoard.jsp페이지를 board주소에 출력한다.
 		return "groupBoard";
 	}
 	
@@ -110,6 +133,8 @@ public class BoardController {
 		model.addAttribute("board_update", dao.getRead(bnum));
 		return "updateboard";
 	}*/
+	
+	//ajax를 활용하기 위한 어노테이션
 	@ResponseBody
 	@RequestMapping(value="updateBoard", method=RequestMethod.GET)
 	public BoardVO board(int bnum, Model model){
@@ -146,10 +171,11 @@ public class BoardController {
 		return "redirect:board";
 	}
 	*/
+	
+	//게시글을 수정
 	@RequestMapping(value="updateBoard", method=RequestMethod.POST, produces="application/json; charset=UTF-8")
 	public String board(BoardVO bv
 			, MultipartFile upload1
-			, String fileDel1
 			, String fileContinue){
 		logger.debug(fileContinue +"bv: {}, mul:{}", bv, upload1);
 		int boardnum = Integer.parseInt(bv.getBoardnum());
--- ReviewCreateBoard/src/main/java/Create/Board/Review/HomeController.java
+++ ReviewCreateBoard/src/main/java/Create/Board/Review/HomeController.java
@@ -8,11 +8,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+/*
+ * 컨트롤러 어노테이션_ 컨트롤러 인터페이스를 구현하지 않고도 컨트롤러를 사용
+       클레스타입에 적용되며, 해당 클레스를 웹 요청을 처리하는 컨트롤러로 사용한다.
+ */
 @Controller
 public class HomeController {
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
+	 */
+	/*
+	 * 요청 URL 매핑을 @RequestMapping을 이용하여 설정
+	 * 클래스나 메서드에 사용되며, 메서드에 이용할 경우 각각의 메서드가 처리할 요청 URL을 명시한다.
+	 * value에 있는 값이 URL값 / method = RequestMethod GET / POST에 따라 받는 방식이 달라진다. 
+	 * return 값에 redirect를 이용하여 URL값을 적으면 해당 URL경로로 이동
+	 * return 값에 string값으로 jsp값을 적으면 해당 URL은 String.jsp페이지가 되어 화면에 출력한다.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
